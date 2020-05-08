7b26db9558c7ae87eb476a8c7dc8749ccdf44398
Abilash Ramesh <52520582+Rame0003@users.noreply.github.com>
2020-05-08 07:13:35 -0500

    Update recipe_finder.py

--- Testcases/recipe_finder.py
+++ Testcases/recipe_finder.py
@@ -25,26 +25,28 @@ def get_user_ing(string, df):
     df = df.append({'ing':user_ing}, ignore_index=True)
     return df
 
-def train_data(df, n):
+def train_data(df):
     vectorizer = TfidfVectorizer(use_idf = True, smooth_idf=True, stop_words = 'english',max_features = 4000)
     ing_vect = vectorizer.fit_transform((df['ing'].values))
     vec = ing_vect.todense()
     X_train_df = vec[:-1]
     y_train_df = df['cuisine'][:-1]
     X_test_df = vec[-1]
-    n = n
+    n = 11
     model = KNeighborsClassifier(n_neighbors = n, weights='uniform', algorithm='auto', metric='minkowski')
     preds = model.fit(X_train_df,y_train_df)
     return X_test_df, model
 
-def get_results(test, model):
+
+def get_results(test, model, n):
     
     predicted_class = model.classes_
     predicted_single_cuisine = model.predict(test)
     predicted_cuisine = model.predict_proba(test)[0]
     match_perc,match_id = model.kneighbors(test)
     pos = np.where(predicted_class == predicted_single_cuisine)
-    print ("The model predicts that the ingredients resembles %s (%f resemblence)" %(predicted_single_cuisine[0], predicted_cuisine[pos]*100))
-    for i in range(len(match_id[0])):
+    print ("The model predicts that the ingredients resembles %s (%f resemblence)\n" %(predicted_single_cuisine[0], predicted_cuisine[pos]*100))
+    for i in range(0, len(match_id[0][:n])):
         print ('Recipe No: %d (%f probable match)'%(match_id[0][i], match_perc[0][i]))
+
 
